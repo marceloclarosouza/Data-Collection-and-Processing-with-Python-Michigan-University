792e28feddb2eb25a8a8f2820b9f50ad434e1088
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-06-09 09:20:53 -0300

    Create exe2.py

---
+++ exe2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 09:11:52 2020
+
+@author: mcsbi
+"""
+#Use indexing to assign the element ‘horse’ to the variable name idx1.
+animals = [['cat', 'dog', 'mouse'], ['horse', 'cow', 'goat'], ['cheetah', 'giraffe', 'rhino']]
+
+idx1 = animals[1][0]
+print(idx1)
+
+
