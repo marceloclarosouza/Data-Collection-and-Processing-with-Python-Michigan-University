88ae327a74630952dc5a71f8e5a639fb8f9af8ca
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-06-12 17:38:09 -0300

    Create w1_4.py

---
+++ w1_4.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 15:44:16 2020
+
+@author: mcsbi
+"""
+
+"""
+Provided is a nested data structure. Follow the instructions in the comments
+ below. Do not hard code.
+"""
+nested = {'data': ['finding', 23, ['exercises', 'hangout', 34]], 'window': ['part', 'whole', [], 'sum', ['math', 'calculus', 'algebra', 'geometry', 'statistics',['physics', 'chemistry', 'biology']]]}
+
+# Check to see if the string data is a key in nested, if it is, assign True to the variable data, otherwise assign False.
+
+for var in nested:
+    if data is key:
+        data = True
+    else:
+        data = False
+        
+print(data)
+        
+        
+
+
+# Check to see if the integer 24 is in the value of the key data, if it is then assign to the variable twentyfour the value of True, otherwise False.
+
+# Check to see that the string 'whole' is not in the value of the key window. If it's not, then assign to the variable whole the value of True, otherwise False.
+
+# Check to see if the string 'physics' is a key in the dictionary nested. If it is, assign to the variable physics, the value of True, otherwise False.
+
+
+
