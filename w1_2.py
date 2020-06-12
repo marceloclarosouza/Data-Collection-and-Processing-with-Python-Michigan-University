dda61c698fff794c489ac3191646c3bcbc0467f7
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-06-12 15:16:29 -0300

    Create w1_2.py

---
+++ w1_2.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 14:16:14 2020
+
+@author: mcsbi
+"""
+
+"""
+ Use in and not in tests to create variables with Boolean values. See 
+ comments for further instructions.
+"""
+lst = [['apple', 'orange', 'banana'], [5, 6, 7, 8, 9.9, 10], ['green', 'yellow', 'purple', 'red']]
+
+#Test to see if 'yellow' is in the third list of lst. Save to variable ``yellow``
+
+if 'yellow' in lst[2]:
+    yellow = True
+else:
+    yellow = False
+    
+print(yellow)
+
+
+#Test to see if 4 is in the second list of lst. Save to variable ``four``
+
+if 4 in lst[1]:
+    four = True
+else:
+    four = False
+print(four)
+
+
+#Test to see if 'orange' is in the first element of lst. Save to variable ``orange``
+
+if "orange" in lst[0][1]:
+    orange = True
+else:
+    orange = False
+print(orange)
