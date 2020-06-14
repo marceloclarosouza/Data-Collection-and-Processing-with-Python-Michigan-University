8188fc6bec739350dca5d1fe10df8530703cf64f
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-06-14 00:32:27 -0300

    Create w1_5.py

---
+++ w1_5.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 09:04:40 2020
+
+@author: mcsbi
+"""
+"""
+The variable nested_d contains a nested dictionary with the gold medal 
+counts for the top four countries in the past three Olympics. Assign the 
+value of Great Britain’s gold medal count from the London Olympics to the 
+variable london_gold. Use indexing. Do not hardcode.
+"""
+
+nested_d = {'Beijing':{'China':51, 'USA':36, 'Russia':22, 'Great Britain':19}, 
+            'London':{'USA':46, 'China':38, 'Great Britain':29, 'Russia':22}, 
+            'Rio':{'USA':35, 'Great Britain':22, 'China':20, 'Germany':13}}
+
+london_gold = 0
+
+for olimp in nested_d['London'].keys():
+    for country in olimp.value():
+        if country =='Great Britain':
+            london_gold = olimp[country]
+            
+print(london_gold)
